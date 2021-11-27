4dc0163e58e8e4300483ea9096a8d76520e0b5e2
Junjiang Li <lij111@miamioh.edu>
2021-11-27 16:56:59 -0500

    Update file handle definition for future flexibility

--- NFS/fhandle.py
+++ NFS/fhandle.py
@@ -1,12 +1,16 @@
+from typing import List
+
+
 class FileHandle:
     """
     File handle struct in the NFSv2 protocol. In the original specification
     of NFSv2 (RFC 1094), file handle is defined as an opaque sequence of bytes,
     meaning the implementation can use these bytes as see fit.
 
-    Since our server will only have one level of directory stored as a filename
-    to char array map, we simply let the file handle be the key to our map
+    Although our server only has a single root directory right now so every file
+    is identifiable by name, we leave the file handle general by storing a path
+    from the root directory for potential future expansion.
     """
 
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, path: List[str]):
+        self.path = path.copy()
