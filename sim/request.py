aa4981e900812324826ccf616b493590bdf44655
Junjiang Li <lij111@miamioh.edu>
2021-12-15 01:40:17 -0500

    Update request types and commutivity check

--- sim/request.py
+++ sim/request.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from NFS.fhandle import FileHandle
+
 
 class Request:
     """
@@ -14,18 +16,13 @@ class Request:
     # Used internally to compare whether two requests commute
     class Type(Enum):
         GETATTR = 0
-        LOOKUP  = 1
-        READ    = 2
-        WRITE   = 3
-
-    # Matrix of commutativity, where indices are operations converted to ints
-    # as specified by the enumeration above.
-    commute_mat = [
-        [True, True, True, False],  # Whether GETATTR commutes with others
-        [True, True, True, False],
-        [True, True, True, False],
-        [False, False, False, False],
-    ]
+        LOOKUP = 1
+        READ = 2
+        WRITE = 3
+        CREATE = 4
+        REMOVE = 5
+        MKDIR = 6
+        RMDIR = 7
 
     def __init__(self, type: Type, func, *args):
         self.type = type
@@ -38,5 +35,94 @@ class Request:
             self.ready = False
             return self.func(*self.args)
 
-    def commutes_with(self, r):
-        return self.commute_mat[self.type.value][r.type.value]
+    def is_file_op(self):
+        return self.type in {
+            self.Type.GETATTR, self.Type.READ, self.Type.WRITE,
+            self.type.CREATE, self.type.REMOVE, self.Type.LOOKUP
+        }
+
+    def _commutes_with(self, r: "Request"):
+        # Debug placeholder to turn off pruning by ensuring all permutation
+        # of events get explored.
+        return False
+
+    def commutes_with(self, r: "Request"):
+        """
+        Tests if two operations commute.
+        :param r: Another request object
+        :return: True if both commute, false otherwise
+        """
+        s = self  # Alias for self to save some typing ;)
+
+        if s.is_file_op() and r.is_file_op():
+            # Both r and s are file operations.
+            if Request.__get_file(s) != Request.__get_file(r):
+                # If operate on different files, then definitely commute
+                return True
+            else:
+                commuting_group = {Request.Type.GETATTR, Request.Type.LOOKUP,
+                                   Request.Type.READ}
+                return s.type in commuting_group and r.type in commuting_group
+
+        # From this point onwards, at least one of r and s is not a
+        # file operation.
+
+        if r.is_file_op():
+            # s is a directory operation and r is a file operation.
+            # Swap r and s and compute commutivity in the next case. This is
+            # possible since commutivity is an equivalence relation.
+            s, r = r, s
+
+        if s.is_file_op():
+            # s is a file operation and r is a directory operation.
+            s_name = Request.__get_file(s)
+            r_name = Request.__get_dir(r)
+            if len(s_name) < len(r_name) or s_name[:len(r_name)] != r_name:
+                # If the file s operates on does not contain the directory r
+                # operates on as a prefix, then cannot commute
+                return True
+            else:
+                # Will not commute regardless of whether r is MKDIR or RMDIR
+                return False
+        else:
+            # Both r and s are directory operations
+            s_name = Request.__get_dir(s)
+            r_name = Request.__get_dir(r)
+            return s_name != r_name  # Commute iff operate on different dirs
+
+    @staticmethod
+    def __get_file(r: "Request") -> str:
+        """
+        Gets the file on which request r operates on
+        :param r: Request object for which r.is_file_op() is True
+        :return: A string representing the absolute path of the file
+        """
+        assert(r.is_file_op())
+
+        if r.type in {Request.Type.LOOKUP, Request.Type.CREATE,
+                      Request.Type.REMOVE}:
+            fhandle, fname = r.args
+            assert(isinstance(fhandle, FileHandle))
+            assert(isinstance(fname, str))
+
+            return '/' + '/'.join(fhandle.path) + '/' + fname
+        else:
+            fhandle = r.args[0]
+            assert(isinstance(fhandle, FileHandle))
+
+            return '/' + '/'.join(fhandle.path)
+
+    @staticmethod
+    def __get_dir(r: "Request") -> str:
+        """
+        Gets the directory on which request r operates on
+        :param r: Request object for which r.is_file_op() is False
+        :return: A string representing the absolute path of the directory
+        """
+        assert(not r.is_file_op())
+
+        fhandle, dirname = r.args
+        assert(isinstance(fhandle, FileHandle))
+        assert(isinstance(dirname, str))
+
+        return '/' + '/'.join(fhandle.path) + '/' + dirname
